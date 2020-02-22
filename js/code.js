0894f9f3f30135f2e819ab7b4b043976a6d8c0d0
ndongamadu <ndongamadou@gmail.com>
2020-02-22 12:33:23 +0000

    stable map

--- js/code.js
+++ js/code.js
@@ -1,106 +1,70 @@
-var map = L.map('map',
+$(document).ready(function(){
+    let hnosLink = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSMmlKxX6PV391tanxo6aKltrtYcF4VTbXtsRtn66Fr4CY_VEjbEpJ9AlZzyIVapdKaOKZwTjyUL8IZ/pub?gid=0&single=true&output=csv';
+    let world = 'data/coordinates.csv';
+    let hnoData, hnoCountries, worldC = '';
 
-    {
-        maxZoom: 5,
-        minZoom: 2
-    });
+    function getLatLon(d){
+        return [d.latitude, d.longitude]
 
-map.setView([9.58, 10.37], 3);
-
-//westlimit=-170.8; southlimit=-84.9; eastlimit=-167.3; northlimit=84.3
-//map.fitBounds([[9.58, 10.37], [0, 0]], 3);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/traffic-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1hZG91MTciLCJhIjoib3NhRnROQSJ9.lW0PVXVIS-j8dGaULTyupg', {
-    attribution: '<a href="http://mapbox.com">Mapbox</a>'
-}).addTo(map);
-
-
-
-
-var geojson;
-
-//var info = L.control();
-//
-//info.onAdd = function (map) {
-//    this._div = L.DomUtil.create('div', 'info');
-//    this.update();
-//    return this._div;
-//};
-
-//info.update = function (props) {
-//
-//    this._div.innerHTML = '<h4>HNO</h4>' + (props ?
-//        '<h5>Availability: ' + (props.hno).toUpperCase() + '</h5><a target="_blank" href="' + props.hdx + '">' + props.name + ' dataset</a><br />' :
-//        'Hover over a state');
-//};
-
-//info.addTo(map);
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        // color: '#ff4000', //'#3182bd', //'#666',
-        //dashArray: '',
-        //fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
     }
 
-    //    info.update(layer.feature.properties);
-    //    //updateKeyFigures(layer.feature.properties);
-}
+    function createMap (hno) {
 
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-}
-//
-//function zoomToFeature(e) {
-//    // map.fitBounds(e.target.getBounds());
-//}
+        var map = L.map('map',
+        {
+            maxZoom: 5,
+            minZoom: 2
+        });
 
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        //click: zoomToFeature
-    });
+        map.setView([9.58, 10.37], 3); 
 
-    if (feature.properties.hdx != '') {
-        layer.bindPopup('<h4>' + feature.properties.name + '</h4><h5><a target="_blank" href="' + feature.properties.hdx + '"> View dataset on HDX </a></h5>');
-    } else {
-        layer.bindPopup('<h4>' + feature.properties.name + '</h4><h5>Dataset not available</h5>');
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/traffic-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1hZG91MTciLCJhIjoib3NhRnROQSJ9.lW0PVXVIS-j8dGaULTyupg', {
+            attribution: '<a href="http://mapbox.com">Mapbox</a>'
+        }).addTo(map); 
+
+        for (var i = 0; i < hno.length; i++) {
+            L.circleMarker(getLatLon(hno[i])).addTo(map)
+                .bindPopup('<h4>'+hno[i].name+'</h4>');
+        }
+
+
+
+    } // createMap()
+
+    function getData () {
+        Promise.all([
+            d3.csv(hnosLink),
+            d3.csv(world)
+        ]).then(function(data){
+            hnoData = [];
+            data[0].forEach(function(d, i){
+                var lat, lon;
+                for (k in data[1]){
+                    if(data[1][k]['ISO3']==d['Country ISO3']){
+                        lat = data[1][k]['Latitude'];
+                        lon = data[1][k]['Longitude'];
+                    }
+
+                }
+                var obj = {
+                    year: d['Year'],
+                    country_code: d['Country ISO3'],
+                    name: d['Country'],
+                    pin: d['PiN'],
+                    dataset: d['HNO dataset'],
+                    latitude: lat,
+                    longitude: lon
+                }
+                hnoData.push(obj);
+            });
+
+            createMap(hnoData);
+            
+
+        });
     }
-}
 
+    getData();
 
-function style(feature) {
-    if (feature.properties.hno == 'yes') {
-        return {
-            fillColor: '#FF493D',
-            weight: 2,
-            opacity: 0.6,
-            color: 'red',
-            fillOpacity: 0.5
-        };
-    } else if (feature.properties.hno == 'no') {
-        return {
-            fillColor: '#BBBBBB',
-            weight: 2,
-            opacity: 0.6,
-            color: 'red',
-            //dashArray: '3',
-            fillOpacity: 0.5
-        };
-    }
+});
 
-}
-
-
-geojson = L.geoJson(hnos, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
